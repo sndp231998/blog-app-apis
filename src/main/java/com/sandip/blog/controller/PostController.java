c1950202130279a456528b2a19a3707b57939262
sndp231998 <sandipchapagain797@gmail.com>
2023-02-28 13:55:28 +0545

    first commit

--- src/main/java/com/sandip/blog/controller/PostController.java
+++ src/main/java/com/sandip/blog/controller/PostController.java
@@ -1,11 +1,15 @@
 package com.sandip.blog.controller;
 
-
+import javax.servlet.http.HttpServletResponse;
 import com.sandip.blog.payloads.*;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
-
+import org.hibernate.engine.jdbc.StreamUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,16 +20,28 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 //import com.sandip.blog.entities.Post;
 import com.sandip.blog.payloads.PostDto;
+import com.sandip.blog.services.FileService;
 import com.sandip.blog.services.PostService;
+
+
 
 @RestController
 @RequestMapping("/api/")
 public class PostController {
+//-----------------------------------------------	
 	@Autowired
 private PostService postService;
+//------------------------------------------------	
+	@Autowired
+	private FileService fileservice;
+//-----------------------------------------------	
+	@Value("${project.image}")
+	private String path;
+//------------------------------------------------	
 	//create
 	@PostMapping("/user/{userId}/category/{categoryId}/posts")
 	public ResponseEntity<PostDto>
@@ -93,6 +109,33 @@ public ResponseEntity<List<PostDto>>getAllPost(){
 			List<PostDto>result=this.postService.searchPosts(keywords);
 			return new ResponseEntity<List<PostDto>>(result,HttpStatus.OK);
 		}
+//-----------------------Uploading_Image_Controller----------------------------
+	@PostMapping("/post/image/upload/{postId}")
+	public ResponseEntity<PostDto>uploadPostImage(
+			@RequestParam("image")MultipartFile image, @PathVariable Integer postId)throws IOException{
+		//first --post xa ki xina ? check garya xina vane yae bata resource not found exception return hunxa
+		PostDto postDto=this.postService.getPostById(postId);
+		
+		String fileName=this.fileservice.uploadImage(path, image);
+		postDto.setImageName(fileName);
+		PostDto updatePost=this.postService.updatePost(postDto, postId);
+		return new ResponseEntity<PostDto>(updatePost,HttpStatus.OK);
+	}
+	
+//------------------------method to serve file/image------------------	
+	
+    @GetMapping(value = "/post/image/{imageName}",produces = MediaType.IMAGE_JPEG_VALUE)
+    public void downloadImage(
+            @PathVariable("imageName") String imageName,
+            HttpServletResponse response
+    ) throws IOException {
+
+        InputStream resource = this.fileservice.getResource(path, imageName);
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        StreamUtils.copy(resource,response.getOutputStream())   ;
+
+    }
+	
 	}
 
 	
